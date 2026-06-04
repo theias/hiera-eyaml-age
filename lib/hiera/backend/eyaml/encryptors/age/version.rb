9309ae4ea97f8c6aa5c3d751f68a19a6d600b55b
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2026-06-04 12:56:03 -0400

    Release 0.2.2

--- lib/hiera/backend/eyaml/encryptors/age/version.rb
+++ lib/hiera/backend/eyaml/encryptors/age/version.rb
@@ -3,7 +3,7 @@ class Hiera
     module Eyaml
       module Encryptors
         module AgeVersion
-          VERSION = "0.2.1".freeze
+          VERSION = "0.2.2".freeze
         end
       end
     end
